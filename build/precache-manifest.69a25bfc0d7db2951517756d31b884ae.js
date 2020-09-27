5d2a1571d447e6df34c5898278516ca50bee5012
Thiago Lima <thiagolima.unb@gmail.com>
2020-09-26 22:29:05 -0300

    first commit

---
+++ build/precache-manifest.69a25bfc0d7db2951517756d31b884ae.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fd560fd4bedac56b4cc014e214f5bdd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d8e7d8019b3691e1cdce",
+    "url": "/static/css/2.52f28872.chunk.css"
+  },
+  {
+    "revision": "432b34cf5088b798c9e3",
+    "url": "/static/css/main.4aa388fc.chunk.css"
+  },
+  {
+    "revision": "d8e7d8019b3691e1cdce",
+    "url": "/static/js/2.397d717d.chunk.js"
+  },
+  {
+    "revision": "3e533a1a18939928aa484d9457ec0229",
+    "url": "/static/js/2.397d717d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "432b34cf5088b798c9e3",
+    "url": "/static/js/main.db03227a.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "03c2733ef6f1ae39570d8d428cb5aaba",
+    "url": "/static/media/13a - Leitura.03c2733e.png"
+  },
+  {
+    "revision": "0fc3a7f54320972d2c943f8a054c5775",
+    "url": "/static/media/13b - Leitura.0fc3a7f5.png"
+  },
+  {
+    "revision": "76cadebb5765a9c1d6c1205e96884a6c",
+    "url": "/static/media/14a - Info.76cadebb.png"
+  },
+  {
+    "revision": "4cc0fa4e2b2570bd8b2307a539e15164",
+    "url": "/static/media/14b - Info.4cc0fa4e.png"
+  },
+  {
+    "revision": "7498fde3c8d9a979d07279da794bd18a",
+    "url": "/static/media/15a - Share.7498fde3.png"
+  },
+  {
+    "revision": "25f7a68797abf8724bc7c22390216c40",
+    "url": "/static/media/15b - Share.25f7a687.png"
+  },
+  {
+    "revision": "448d2af475c882b0fe9ec156f6eaa69b",
+    "url": "/static/media/2018d.448d2af4.png"
+  },
+  {
+    "revision": "8f43814c951c3142980cb836d7a3701c",
+    "url": "/static/media/FAC_Marca_RGB_PB horizontal a.8f43814c.png"
+  },
+  {
+    "revision": "e5e838af320144c7212c5b6b02eb9ff3",
+    "url": "/static/media/SECEC_HORIZONTAL_PRETO-BRANCO_principal CMYK-01a.e5e838af.png"
+  }
+]);
